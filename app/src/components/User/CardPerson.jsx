ac82fd643ec4c2308b9c105d82da1a07c51ec3b2
Matheus Folitti <matheusfoliatti@gmail.com>
2023-05-30 07:03:41 -0300

    base

---
+++ app/src/components/User/CardPerson.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import stylePerson from "./CardPerson.module.css";
+
+export function CardPerson({ cover, avatar, name, office }) {
+  return (
+    <div className={stylePerson.card}>
+      <img src={cover} alt="Cover" />
+      <img className={stylePerson.avatar} src={avatar} alt="Avatar" />
+      <p className={stylePerson.p1}>{name}</p>
+      <p className={stylePerson.p2}>{office}</p>
+
+      <div className={stylePerson.spans}>
+      <span >#CSS</span>
+      <span >#HTML</span>
+      <span >#REACT</span>
+      <span >#NATIVE</span>
+      </div>
+      
+    </div>
+  );
+}
